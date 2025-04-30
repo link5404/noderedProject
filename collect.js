b26940a0e9848e4cdfea744a4f3b127fde670e83
joeykozohar <joeykozohar@vt.edu>
2025-04-30 12:41:02 -0400

    collect method

---
+++ collect.js
@@ -0,0 +1,41 @@
+module.exports = function(RED) {
+    function CollectNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        
+        // Store collect parameters from node configuration
+        this.collectExpression = config.collectExpression;
+        
+        node.on('input', function(msg) {
+
+            if (msg.payload && typeof msg.payload === 'object') {
+                var collectMethod = {
+                    "type": "collect",
+                };
+
+                // Add collect method to Methods array
+                msg.payload.Methods.push(collectMethod);
+
+                // Send the modified message
+                node.send(msg);
+
+            } else {
+                node.error("Payload must be a valid JSON object", msg);
+            }
+        });
+    }
+    
+    RED.nodes.registerType("spark-collect", CollectNode, {
+        defaults: {
+            name: { value: "" },
+            collectExpression: { value: "" }
+        },
+        category: "spark",
+        color: "#E6E0F8",
+        icon: "collect.png",
+        label: function() {
+            return this.name || "spark-collect";
+        },
+        paletteLabel: "collect"
+    });
+}
